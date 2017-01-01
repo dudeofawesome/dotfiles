f637b23d5ec34fc7de98a747a1315be9b476c8dd
Louis Orleans <louis@orleans.io>
2017-01-01 13:13:35 -0700

    ✨ Adding VPN config

---
+++ settings/vpn.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+source ../libs.sh
+
+echo "###############################################################################"
+echo "# VPN"
+echo "###############################################################################"
+
+echo ""
+echo "Installing PIA configs"
+pushd ~/Downloads
+wget "https://www.privateinternetaccess.com/openvpn/openvpn.zip"
+unzip openvpn.zip
+cd openvpn
+tput bel
+open "US California.ovpn"
+tput bel
+open "US East.ovpn"
+tput bel
+open "US Midwest.ovpn"
+tput bel
+open "Switzerland.ovpn"
+popd
