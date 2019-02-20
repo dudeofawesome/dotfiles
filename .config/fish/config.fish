de0623b69e8e4a3e90f93fe3982d1ea9bf876763
Louis Orleans <louis@orleans.io>
2019-02-20 15:16:07 -0800

    ✨ update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,16 @@
-alias up="cd .."
-alias back="cd -"
 function cl -d "cd into directory and ls"
   cd $argv
   ls
 end
-alias l="ls -la"
-alias reload="source ~/.config/fish/config.fish"
+abbr -a l ls -la
+abbr -a reload source ~/.config/fish/config.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 set -x ANDROID_HOME "/Users/"(whoami)"/Library/Android/sdk"
+set -x ANDROID_SDK_ROOT "/Users/"(whoami)"/Library/Android/sdk"
 
-set PATH $PATH ~/Library/Android/sdk/tools ~/Library/Android/sdk/platform-tools
+set PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $HOME/Library/Developer/flutter/bin $HOME/.fastlane/bin
 
-set -x EDITOR /usr/local/bin/vim
+set -Ux EDITOR vim
 
