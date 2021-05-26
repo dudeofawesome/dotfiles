87ba7c185cee02da28927d63729fee0375a2b0c4
Louis Orleans <dudeofawesome@edgar-plotter.localdomain>
2021-05-26 03:40:07 -0700

    ✨ add lblk and path updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ function cl -d "cd into directory and ls"
   ls
 end
 abbr -a l ls -lha
+abbr -a lblk lsblk --output NAME,SIZE,RM,FSTYPE,UUID,MOUNTPOINT
+
 abbr -a reload source ~/.config/fish/config.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
@@ -10,7 +12,16 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 set -x ANDROID_HOME "/Users/"(whoami)"/Library/Android/sdk"
 set -x ANDROID_SDK_ROOT "/Users/"(whoami)"/Library/Android/sdk"
 
-set PATH $PATH /usr/local/bin $HOME/.local/bin $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $HOME/Library/Developer/flutter/bin $HOME/.fastlane/bin $HOME/.gem/ruby/2.6.0/bin
+fish_add_path /usr/local/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $ANDROID_HOME/tools
+fish_add_path $ANDROID_HOME/tools/bin
+fish_add_path $ANDROID_HOME/platform-tools
+fish_add_path $HOME/Library/Developer/flutter/bin
+fish_add_path $HOME/.fastlane/bin
+fish_add_path $HOME/.gem/ruby/2.6.0/bin
+
+
 set -g fish_user_paths "/usr/local/sbin" "/usr/local/opt/ruby/bin" $fish_user_paths
 
 set -Ux EDITOR vim
