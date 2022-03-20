a7ff141e4c08ed08e6cb075cc68016c55a641f09
Louis Orleans <louis@orleans.io>
2022-03-20 11:36:05 -0700

    🔧 open Google Meet in Chrome

---
+++ settings/.finicky.js
@@ -0,0 +1,12 @@
+// Use https://finicky-kickstart.now.sh to generate basic configuration
+// Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
+
+module.exports = {
+  defaultBrowser: 'Firefox',
+  handlers: [
+    {
+      match: ['meet.google.com/*'],
+      browser: 'Google Chrome',
+    },
+  ],
+};
