733e15dc176d54970cfe6694637b000af7a39dae
Louis Orleans <louis@orleans.io>
2022-10-12 01:56:47 -0700

    🐛 fix glcoud existence test

--- home/.config/fish/functions/_tide_item_gcloud.fish
+++ home/.config/fish/functions/_tide_item_gcloud.fish
@@ -1,5 +1,5 @@
 function _tide_item_gcloud
-  if test -x (which gcloud)
+  if type -q gcloud
     set -l gcp_project_id (gcloud config get project 2>/dev/null)
     _tide_print_item gcloud $tide_gcloud_icon' ' "$gcp_project_id"
   end
