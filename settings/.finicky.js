69688b5691b9012df902d5cae6bea965dfdc6f10
Louis Orleans <louis@orleans.io>
2023-07-24 14:58:04 -0700

    ♻️ sort handlers

--- settings/.finicky.js
+++ settings/.finicky.js
@@ -5,34 +5,47 @@
 module.exports = {
   defaultBrowser: 'Firefox',
   handlers: [
-    {
-      match: ['meet.google.com/*'],
-      browser: 'Google Chrome',
-      // browser: '/Users/dudeofawesome/Applications/Chrome Apps.localized/Google Meet.app',
-    },
-    {
-      match: [
-        '*cloud.google.com/*',
-        'redirect.teleparty.com/join/*',
-        'https://spectra.loggly.com/*',
-      ],
-      browser: 'Google Chrome',
-    },
-    {
-      match: ['*.zoom.us/j/*'],
-      browser: '/Applications/zoom.us.app',
-    },
-    {
-      match: ['music.apple.com/*'],
-      browser: '/System/Applications/Music.app',
-    },
-    {
-      match: ['open.spotify.com/*'],
-      browser: '/Applications/Spotify.app',
-    },
-    {
-      match: ['https://prod.liveshare.*.visualstudio.com/join?*'],
-      browser: '/Applications/Visual Studio Code.app',
-    },
-  ],
+    /* Performance */ [
+      {
+        match: [
+          '*cloud.google.com/*',
+          'redirect.teleparty.com/join/*',
+          'https://spectra.loggly.com/*',
+        ],
+        browser: 'Google Chrome',
+      },
+    ],
+    /* Music */ [
+      {
+        match: ['music.apple.com/*'],
+        browser: '/System/Applications/Music.app',
+      },
+      {
+        match: ['open.spotify.com/*'],
+        browser: '/Applications/Spotify.app',
+      },
+    ],
+    /* Video Conferencing */ [
+      {
+        match: ['meet.google.com/*'],
+        browser: 'Google Chrome',
+        // browser: '/Users/dudeofawesome/Applications/Chrome Apps.localized/Google Meet.app',
+      },
+      {
+        match: ['*.zoom.us/j/*'],
+        browser: '/Applications/zoom.us.app',
+      },
+      // Slack seems to need the URL to be rewritten somehow
+      // {
+      //   match: ['slack://*'],
+      //   browser: '/Applications/Slack.app',
+      // },
+    ],
+    /* Other */ [
+      {
+        match: ['https://prod.liveshare.*.visualstudio.com/join?*'],
+        browser: '/Applications/Visual Studio Code.app',
+      },
+    ],
+  ].flat(),
 };
